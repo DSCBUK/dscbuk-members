6837390c29ce56d3534fa8800422998bd5d398c6
Tim <timothyolaleke@gmail.com>
2018-08-31 17:55:19 +0100

    Implemented PWA and added Bulma package

--- src/main.js
+++ src/main.js
@@ -9,3 +9,14 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+  .then(function(reg) {
+    // registration worked
+    console.log('Service Worker Registration succeeded.');
+  }).catch(function(error) {
+    // registration failed
+    console.log('Service Worker Registration succeeded.');
+  });
+}
