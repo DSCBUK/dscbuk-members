e46c0d8034799224f1b5ce1da5fc400d57699842
Tim <timothyolaleke@gmail.com>
2018-09-15 10:13:37 +0100

    Added emall

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="DSC logo" src="../assets/dsc.png" width="50px" height="50px">
     <Members msg="Welcome to DSCBUK"/>
+    <pre> Send us a mail: hi@dscbuk.club </pre>
   </div>
 </template>
 
